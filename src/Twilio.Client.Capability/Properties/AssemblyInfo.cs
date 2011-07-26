f922d68765f271f88405f99a60c221af305e43e5
johnsheehan <johnsheehan@gmail.com>
2011-07-25 17:26:24 -0700

    adding Twilio.Client.Capability and bumping version to 3.1.0

---
+++ src/Twilio.Client.Capability/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Twilio.Client.Capability")]
+[assembly: AssemblyDescription("Twilio Client capability token generator for use with Twilio.js.")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e9e02cd4-b3fe-4e35-9aef-43936331f485")]
---
+++ src/Twilio.Client.Capability/TwilioCapability.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using JWT;
+
+namespace Twilio
+{
+	public class TwilioCapability
+	{
+		string _accountSid;
+		string _authToken;
+		string _clientName;
+
+		List<ScopeURI> _scopes;
+
+		public TwilioCapability(string accountSid, string authToken)
+		{
+			_accountSid = accountSid;
+			_authToken = authToken;
+			_scopes = new List<ScopeURI>();
+		}
+
+		void Allow(string service, string capability, object prms)
+		{
+			// add to scope
+			_scopes.Add(new ScopeURI(service, capability, prms));
+		}
+
+		public void AllowClientIncoming(string clientName)
+		{
+			// clientName must be a non-zero length alphanumeric string
+			if (Regex.IsMatch(clientName, @"\W"))
+			{
+				throw new ArgumentException("Only alphanumeric characters allowed in client name.");
+			}
+
+			if (string.IsNullOrEmpty(clientName))
+			{
+				throw new ArgumentException("Client name must not be a zero length string or NULL.");
+			}
+
+			_clientName = clientName;
+			Allow("client", "incoming", null);
+		}
+
+		public void AllowClientOutgoing(string applicationSid)
+		{
+			AllowClientOutgoing(applicationSid, null);
+		}
+
+		public void AllowClientOutgoing(string applicationSid, object appParams)
+		{
+			Allow("client", "outgoing", new { appSid = applicationSid, appParams });
+		}
+
+		public string GenerateToken()
+		{
+			return GenerateToken(3600);
+		}
+
+		public string GenerateToken(int ttlSeconds)
+		{
+			var scope = string.Join(" ", _scopes.Select(s => s.ToString(_clientName)).ToArray());
+
+			var payload = new
+			{
+				iss = _accountSid,
+				exp = ConvertToUnixTimestamp(DateTime.UtcNow.AddSeconds(ttlSeconds)),
+				scope
+			};
+
+			return JsonWebToken.Encode(payload, _authToken, JwtHashAlgorithm.HS256);
+		}
+
+		static int ConvertToUnixTimestamp(DateTime date)
+		{
+			DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+			TimeSpan diff = date - origin;
+			return (int)Math.Floor(diff.TotalSeconds);
+		}
+	}
+}
