09265a5b21712c3c7b5415e68ad1ff2c1c93cbb0
Joel Martinez <joelmartinez@gmail.com>
2012-07-07 09:50:30 -0400

    Added VoiceRequest class. Also created a common base class

--- src/Twilio.Mvc/SmsRequest.cs
+++ src/Twilio.Mvc/SmsRequest.cs
@@ -1,25 +1,12 @@
-﻿
-namespace Twilio.Mvc
+﻿namespace Twilio.Mvc
 {
     /// <summary>
     /// This class can be used as the parameter on your SMS action. Incoming parameters will be bound here.
     /// </summary>
     /// <remarks>http://www.twilio.com/docs/api/twiml/sms/twilio_request</remarks>
-    public class SmsRequest
+    public class SmsRequest : TwilioRequest
     {
         public string SmsSid { get; set; }
-        public string AccountSid { get; set; }
-        public string From { get; set; }
-        public string To { get; set; }
         public string Body { get; set; }
-
-        public string FromCity { get; set; }
-        public string FromState { get; set; }
-        public string FromZip { get; set; }
-        public string FromCountry { get; set; }
-        public string ToCity { get; set; }
-        public string ToState { get; set; }
-        public string ToZip { get; set; }
-        public string ToCountry { get; set; }
     }
 }
