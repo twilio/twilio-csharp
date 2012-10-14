17fc386770f5545719c3b45d7a119b31025da0a3
Devin Rader <devin@twilio.com>
2012-10-14 16:11:52 -0400

    Usage API updates
    - Added Usage API
    - Updated solution to use nuget pacakge restore
    - General nuget package updating

---
+++ src/Twilio.Api/Model/UsageTriggerResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Twilio
+{
+    /// <summary>
+    /// Twilio API call result with paging information
+    /// </summary>
+    public class UsageTriggerResult : TwilioListBase
+    {
+        /// <summary>
+        /// List of UsageTrigger resources returned by API
+        /// </summary>
+        public List<UsageTrigger> UsageTriggers { get; set; }
+    }
+}
