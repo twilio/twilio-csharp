45fff43ee3ff9b8f1f75d68da6713dcc4a9e64cb
Devin Rader <devin@twilio.com>
2012-12-03 16:00:13 -0500

    Fixed the ValidationRequestAttribute
    Previously the ValicationRequestAttribute would allow execution of the
    Action method to continue, even if the validation failed, and even
    though it returned a 403.  The attribute has now been changed so that if
    validation fails, a specific HttpStatusCodeResult (with 403) is returned
    wihhc prevents the action from continuing to execute.

---
+++ src/Twilio.Mvc/HttpStatusCodeResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Twilio
+{
+    public class HttpStatusCodeResult : ActionResult
+    {
+        public HttpStatusCodeResult(HttpStatusCode statusCode)
+        {
+            StatusCode = (int)statusCode;
+        }
+
+        public int StatusCode { get; private set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            context.HttpContext.Response.StatusCode = StatusCode;
+        }
+    }
+}
