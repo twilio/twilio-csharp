2716d03d78def447fc82bda01b2e623e6116229a
Kevin Burke <kevin@twilio.com>
2012-12-26 21:16:06 -0800

    Add AccountOptions class to update an account.

---
+++ src/Twilio.Api/Model/AccountOptions.cs
@@ -0,0 +1,15 @@
+namespace Twilio
+{
+	/// <summary>
+	/// Available options to include when editing an account
+	/// </summary>
+	public class AccountOptions 
+    {
+
+        public string Status { get; set; }
+
+        public string FriendlyName { get; set; }
+
+    }
+}
+
