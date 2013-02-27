86c6a0f1081fe91c8805d108ee0ad85715ce8262
Devin Rader <devin@twilio.com>
2013-02-27 17:28:01 -0500

    Adding Leave very to fix #68

---
+++ src/Twilio.Twiml/Verbs/Leave.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Twilio.TwiML;
+using System.Xml.Linq;
+
+namespace Twilio.Twiml.Verbs
+{
+    /// <summary>
+    /// Transfers control of a call that is in a queue so that the caller exits the queue and execution continues with the next verb after the original Enqueue
+    /// </summary>
+    public class Leave : ElementBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the Leave class.
+        /// </summary>
+        /// <param name="name"></param>
+        public Leave()
+        {
+            Element = new XElement("Leave");
+        }
+    }
+}
