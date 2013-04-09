77c704bb114a04dfccf9ac656a488c24c0cd4a14
Devin Rader <devin@twilio.com>
2013-04-09 13:52:12 -0300

    Update Credentials.cs

--- src/Twilio.Api.Tests.Integration/Credentials.cs
+++ src/Twilio.Api.Tests.Integration/Credentials.cs
@@ -8,10 +8,10 @@ namespace Twilio.Api.Tests.Integration
 {
     public static class Credentials
     {
-        public static string TestAccountSid = "ACa9dc1012ce89bcd471791338c756432d";
-        public static string TestAuthToken = "c7e70e288326541defe0d6f18a5f72ee";
+        public static string TestAccountSid = "[YOUR_TEST_ACCOUNT_SID]";
+        public static string TestAuthToken = "[YOUR_TEST_AUTH_TOKEN]";
 
-        public static string AccountSid = "ACc5cd732b74cc41ce4117f6751791c1dd";
-        public static string AuthToken = "80cb14436fb3e4f55822ba80421bcafb";
+        public static string AccountSid = "[YOUR_ACCOUNT_SID]";
+        public static string AuthToken = "[YOUR_AUTH_TOKEN]";
     }
 }
