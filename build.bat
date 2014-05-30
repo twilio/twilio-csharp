2691f4821a2d931fdfefac40dfccd2c9995e6e1d
Devin Rader <devin@twilio.com>
2014-05-29 21:17:53 -0400

    manually run package restore

--- build.bat
+++ build.bat
@@ -14,7 +14,7 @@ REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Twilio.2013.sln /p:C
 REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Twilio.2013.sln /p:Configuration=PCL /t:Clean
 
 REM Package restore
-cmd /c %nuget% restore packages.config -NoCache -NonInteractive
+cmd /c %nuget% restore packages.config -PackagesDirectory src\packages -Verbosity detailed -NoCache -NonInteractive
 
 REM Build Source
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Twilio.2013.sln /p:Configuration=FX35 /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:true /p:BuildInParallel=true /p:RestorePackages=true /t:Rebuild
