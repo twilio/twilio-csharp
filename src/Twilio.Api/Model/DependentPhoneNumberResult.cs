e2b17903ad47954d3f211ad10c6389a2955ef494
Sam Kimbrel <skimbrel@twilio.com>
2014-09-29 14:06:45 -0700

    Add Address models

---
+++ src/Twilio.Api/Model/DependentPhoneNumberResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twilio
+{
+	/// <summary>
+	/// Twilio API call result with paging information.
+	/// </summary>
+	public class DependentPhoneNumberResult : TwilioListBase
+	{
+		public List<DependentPhoneNumber> DependentPhoneNumbers { get; set; }
+	}
+}
+
