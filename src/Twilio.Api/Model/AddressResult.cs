ebc0a4fed573ab7f33f156bd4fde67daf4d4bd73
Sam Kimbrel <skimbrel@twilio.com>
2014-09-29 14:06:45 -0700

    Add Address models

---
+++ src/Twilio.Api/Model/AddressResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Twilio
+{
+	/// <summary>
+	/// Twilio API call result with paging information.
+	/// </summary>
+	public class AddressResult : TwilioListBase
+	{
+		/// <summary>
+		/// List of Address results returned from Twilio API call.
+		/// </summary>
+		public List<Address> Addresses { get; set; }
+	}
+}
+
