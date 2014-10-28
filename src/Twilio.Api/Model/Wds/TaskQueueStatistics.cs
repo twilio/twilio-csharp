9e93552cf66040157ce145a32b8912f2491093e0
Alexandre Payment <apayment@twilio.com>
2014-10-28 11:06:46 -0700

    Start statistics work

---
+++ src/Twilio.Api/Model/Wds/TaskQueueStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Twilio.Wds
+{
+    public class TaskQueueStatistics
+    {
+        public string AccountSid { get; set; }
+        public string WorkspaceSid { get; set; }
+        public string TaskQueueSid { get; set; }
+
+        class TaskQueueRealtimeStatistics
+        {
+            public string LongestTaskWaitingSid { get; set; }
+            public int? LongestTaskWaitingAge { get; set; }
+            public int? TotalTasks { get; set; }
+            // #TODO TasksByStatus
+            // #TODO ActivityStatistics
+            public int? TotalEligibleWorkers { get; set; }
+            public int? TotalAvailableWorkers { get; set; }
+        }
+    }
+}
+
