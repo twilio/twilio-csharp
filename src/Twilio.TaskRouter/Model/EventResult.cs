3b0b77a4b66863bfe475bd095e04b85b0b199e00
Alexandre Payment <apayment@twilio.com>
2015-02-10 17:15:30 -0800

    Implements taskrouter events

---
+++ src/Twilio.TaskRouter/Model/EventResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twilio.TaskRouter
+{
+    /// <summary>
+    /// Twilio API call result with paging information.
+    /// </summary>
+    public class EventResult : TwilioListBase
+    {
+        /// <summary>
+        /// Gets or sets the events.
+        /// </summary>
+        public List<Event> Events { get; set; }
+    }
+}
+
