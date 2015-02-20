9902671a8bbaf473407e38b51c76ccde7a48621f
Alexandre Payment <apayment@twilio.com>
2015-02-20 14:24:28 -0800

    Use NextGenListBase

--- src/Twilio.TaskRouter/Model/EventResult.cs
+++ src/Twilio.TaskRouter/Model/EventResult.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Twilio.Model;
+
 namespace Twilio.TaskRouter
 {
     /// <summary>
     /// Twilio API call result with paging information.
     /// </summary>
-    public class EventResult : TwilioListBase
+    public class EventResult : NextGenListBase
     {
         /// <summary>
         /// Gets or sets the events.
