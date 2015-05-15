02cc38a2d74725a0687fcb609b319cb3fea8acae
Sam Kimbrel <skimbrel@twilio.com>
2015-05-15 13:29:26 -0700

    Add Conversations client and resources.

---
+++ src/Twilio.Conversations/Model/ConversationResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Twilio.Model;
+
+namespace Twilio.Conversations.Model
+{
+    public class ConversationResult : NextGenListBase
+    {
+        /// <summary>
+        /// List of Conversations retrieved from the API.
+        /// </summary>
+        public List<Conversation> Conversations { get; set; }
+    }
+}
---
+++ src/Twilio.Conversations/Participants.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using RestSharp.Extensions;
+using RestSharp.Validation;
+
+using Twilio.Conversations.Model;
+
+namespace Twilio.Conversations
+{
+    public partial class ConversationsClient
+    {
+        public Participant GetParticipant(string conversationSid, string participantSid)
+        {
+            Require.Argument("ConversationSid", conversationSid);
+            Require.Argument("ParticipantSid", participantSid);
+            var request = new RestRequest();
+            request.Resource = "Conversations/{ConversationSid}/Participants/{ParticipantSid}";
+
+            request.AddUrlSegment("ConversationSid", conversationSid);
+            request.AddUrlSegment("ParticipantSid", participantSid);
+
+            return Execute<Participant>(request);
+        }
+
+        public ParticipantResult ListParticipants(string conversationSid)
+        {
+            Require.Argument("ConversationSid", conversationSid);
+
+            var request = new RestRequest();
+            request.Resource = "Conversations/{ConversationSid}/Participants";
+
+            request.AddUrlSegment("ConversationSid", conversationSid);
+
+            return Execute<ParticipantResult>(request);
+        }
+
+        public Participant DisconnectParticipant(string conversationSid, string participantSid)
+        {
+            Require.Argument("ConversationSid", conversationSid);
+            Require.Argument("ParticipantSid", participantSid);
+
+            var request = new RestRequest(Method.POST);
+            request.Resource = "Conversations/{ConversationSid}/Participants/{ParticipantSid}";
+
+            request.AddUrlSegment("ConversationSid", conversationSid);
+            request.AddUrlSegment("ParticipantSid", participantSid);
+
+            request.AddParameter("Status", "disconnected");
+            return Execute<Participant>(request);
+        }
+
+        public Participant AddParticipant(string conversationSid, string to, string from)
+        {
+            Require.Argument("ConversationSid", conversationSid);
+            Require.Argument("To", to);
+            Require.Argument("From", from);
+
+            var request = new RestRequest(Method.POST);
+            request.Resource = "Conversations/{ConversationSid}/Participants";
+
+            request.AddUrlSegment("ConversationSid", conversationSid);
+
+            request.AddParameter("To", to);
+            request.AddParameter("From", from);
+
+            return Execute<Participant>(request);
+        }
+    }
+}
