69503dba61265a934fc58a3040575e6615cf5d9f
Sam Kimbrel <skimbrel@twilio.com>
2015-05-18 13:52:53 -0700

    Add Conversation deserialize test

---
+++ src/Twilio.Conversations.Tests/Model/ConversationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Deserializers;
+
+using Twilio.Conversations.Model;
+
+namespace Twilio.Conversations.Tests.Model
+{
+    [TestFixture]
+    public class ConversationTests
+    {
+        [Test]
+        public void testDeserializeResponse()
+        {
+            var doc = File.ReadAllText(Path.Combine("Resources", "conversation.json"));
+            var json = new JsonDeserializer();
+            var output = json.Deserialize<Conversation>(new RestResponse { Content = doc });
+
+            Assert.NotNull(output);
+            Assert.NotNull(output.DateCreated);
+            Assert.AreEqual("completed", output.Status);
+        }
+    }
+}
+
