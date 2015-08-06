08d7c77700b98da27688f9e72ff374fbef9293d9
Justin Witz <jwitz@twilio.com>
2015-08-06 15:12:07 -0700

    Add enqueuing a task with priority and timeout

--- src/Twilio.Twiml.Tests/EnqueueTests.cs
+++ src/Twilio.Twiml.Tests/EnqueueTests.cs
@@ -46,5 +46,24 @@ namespace Twilio.Twiml.Tests
 
             Assert.IsTrue(IsValidTwiML(response.ToXDocument()));
         }
+
+        [TestMethod]
+        public void Can_Generate_Enqueue_With_Options_And_TaskAttributes_And_Priority_And_Timeout()
+        {
+            var response = new TwilioResponse();
+            response.Enqueue(new { action = "example.xml", method = "GET", waitUrl = "wait.xml", waitUrlMethod = "GET", workspaceSid = "WSXXXXX" }, "{'task':'attributes'}", new {priority="10", timeout="30"});
+
+            Assert.IsTrue(IsValidTwiML(response.ToXDocument()));
+        }
+
+        [TestMethod]
+        public void Can_Generate_Enqueue_With_Task()
+        {
+            var response = new TwilioResponse();
+            var task = new Task("{'task':'attributes'}", new {priority = "10", timeout = "30"});
+            response.EnqueueTask(task);
+
+            Assert.IsTrue(IsValidTwiML(response.ToXDocument()));
+        }
     }
 }
