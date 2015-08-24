d413c2ae7b14b175ca4407ddb4728c266bb553d8
mplacona <marcos.placona@gmail.com>
2015-08-24 14:50:52 +0100

    Make the /SMS route obsolete in favour of the /Message route

--- src/Twilio.Api/Sms.cs
+++ src/Twilio.Api/Sms.cs
@@ -12,6 +12,7 @@ namespace Twilio
 		/// Makes a GET request to an SMSMessage Instance resource.
 		/// </summary>
 		/// <param name="smsMessageSid">The Sid of the message to retrieve</param>
+        [Obsolete("Use GetMessage instead.")]
         public virtual SMSMessage GetSmsMessage(string smsMessageSid)
 		{
 			var request = new RestRequest();
@@ -26,6 +27,7 @@ namespace Twilio
 		/// The list includes paging information.
 		/// Makes a GET request to the SMSMessage List resource.
 		/// </summary>
+        [Obsolete("Use ListMessages instead.")]
         public virtual SmsMessageResult ListSmsMessages()
 		{
 			return ListSmsMessages(null, null, null, null, null);
@@ -40,6 +42,7 @@ namespace Twilio
 		/// <param name="dateSent">(Optional) The date the message was sent (GMT)</param>
 		/// <param name="pageNumber">(Optional) The page to start retrieving results from</param>
 		/// <param name="count">(Optional) The number of results to retrieve</param>
+        [Obsolete("Use ListMessages instead.")]
         public virtual SmsMessageResult ListSmsMessages(string to, string from, DateTime? dateSent, int? pageNumber, int? count)
 		{
 			var request = new RestRequest();
@@ -61,6 +64,7 @@ namespace Twilio
 		/// <param name="from">The phone number to send the message from. Must be a Twilio-provided or ported local (not toll-free) number. Validated outgoing caller IDs cannot be used.</param>
 		/// <param name="to">The phone number to send the message to. If using the Sandbox, this number must be a validated outgoing caller ID</param>
 		/// <param name="body">The message to send. Must be 160 characters or less.</param>
+        [Obsolete("Use SendMessage instead.")]
         public virtual SMSMessage SendSmsMessage(string from, string to, string body)
 		{
 			return SendSmsMessage(from, to, body, string.Empty);
@@ -74,6 +78,7 @@ namespace Twilio
 		/// <param name="to">The phone number to send the message to. If using the Sandbox, this number must be a validated outgoing caller ID</param>
 		/// <param name="body">The message to send. Must be 160 characters or less.</param>
 		/// <param name="statusCallback">A URL that Twilio will POST to when your message is processed. Twilio will POST the SmsSid as well as SmsStatus=sent or SmsStatus=failed</param>
+        [Obsolete("Use SendMessage instead.")]
         public virtual SMSMessage SendSmsMessage(string from, string to, string body, string statusCallback)
 		{
             return SendSmsMessage(from, to, body, statusCallback, string.Empty);
@@ -88,6 +93,7 @@ namespace Twilio
         /// <param name="body">The message to send. Must be 160 characters or less.</param>
         /// <param name="statusCallback">A URL that Twilio will POST to when your message is processed. Twilio will POST the SmsSid as well as SmsStatus=sent or SmsStatus=failed</param>
         /// <param name="applicationSid">Twilio will POST SmsSid as well as SmsStatus=sent or SmsStatus=failed to the URL in the SmsStatusCallback property of this Application. If the StatusCallback parameter above is also passed, the Application's SmsStatusCallback parameter will take precedence.</param>
+        [Obsolete("Use SendMessage instead.")]
         public virtual SMSMessage SendSmsMessage(string from, string to, string body, string statusCallback, string applicationSid)
         {
             Require.Argument("from", from);
@@ -109,7 +115,7 @@ namespace Twilio
 		/// Retrieve the details for a specific ShortCode instance.
 		/// Makes a GET request to a ShortCode Instance resource.
 		/// </summary>
-		/// <param name="shortCodeSid">The Sid of the ShortCode resource to return</param>
+		/// <param name="shortCodeSid">The Sid of the ShortCode resource to return</param>        
         public virtual SMSShortCode GetShortCode(string shortCodeSid)
 		{
 			Require.Argument("shortCodeSid", shortCodeSid);
