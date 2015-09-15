334870f8aa684f2d27f62d493cd9247ef2252e0f
Senthil Ramakrishnan <sramakrishnan@twilio.com>
2015-09-15 10:37:36 -0700

    Fixed the nesting of comment tags

--- src/Twilio.Pricing/Model/MessagingCountryResult.cs
+++ src/Twilio.Pricing/Model/MessagingCountryResult.cs
@@ -8,13 +8,12 @@ namespace Twilio.Pricing
     /// <summary>
     /// Represents the list of countries where Twilio Messaging services
     /// are available.
+    /// </summary>
     /// <remarks>
     /// Note that the returned Country objects will not have
     /// pricing information populated. To retrieve prices for a specific
-    /// country, request it with the <code>GetMessagingCountry</code>
-    /// method.
+    /// country, request it with the GetMessagingCountry method.
     /// </remarks>
-    /// </summary>
     public class MessagingCountryResult : NextGenListBase
     {
         public List<Country> Countries { get; set; }
