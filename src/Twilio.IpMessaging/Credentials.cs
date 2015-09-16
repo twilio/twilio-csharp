6f1d204d8fcd5500e59eaa384873fd34607dc113
Senthil Ramakrishnan <sramakrishnan@twilio.com>
2015-09-16 10:04:38 -0700

    Adding Credentials service wrapper and models

---
+++ src/Twilio.IpMessaging/Credentials.cs
@@ -0,0 +1,101 @@
+using System;
+using RestSharp;
+using RestSharp.Extensions;
+using RestSharp.Validation;
+
+namespace Twilio.IpMessaging
+{
+    public partial class IpMessagingClient
+    {
+        /// <summary>
+        /// Retrieves all the Credentials belonging to a Service Sid.
+        /// </summary>
+        public virtual CredentialResult ListCredentials()
+        {
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/Credentials";
+
+            return Execute<CredentialResult>(request);
+        }
+
+        /// <summary>
+        /// Retrieves the Credential by Credential Sid.
+        /// </summary>
+        /// <param name="credentialSid">Credential Sid</param>
+        public virtual Credential GetCredential(string credentialSid)
+        {
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/Credentials/{CredentialSid}";
+            request.AddUrlSegment("CredentialSid", credentialSid);
+
+            return Execute<Credential>(request);
+        }
+
+        /// <summary>
+        /// Creates a Credential.
+        /// </summary>
+        /// <param name="type">Credential type</param>
+        /// <param name="friendlyName">Friendly Name for the Credential</param>
+        /// <param name="certificate">Certificate</param>
+        /// <param name="privateKey">Private Key</param>
+        /// <param name="sandbox">Flag denotes if it is Sandbox or not</param>
+        /// <param name="apiKey">API Key</param>
+        public virtual Credential CreateCredential(string type,
+          string friendlyName, string certificate, string privateKey,
+          string sandbox, string apiKey)
+        {
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/Credentials";
+
+            request.AddParameter("Type", type);
+            request.AddParameter("FriendlyName", friendlyName);
+            request.AddParameter("Certificate", certificate);
+            request.AddParameter("PrivateKey", privateKey);
+            request.AddParameter("Sandbox", sandbox);
+            request.AddParameter("ApiKey", apiKey);
+
+            return Execute<Credential>(request);
+        }
+
+        /// <summary>
+        /// Modifies a Credential.
+        /// </summary>
+        /// <param name="type">Credential type</param>
+        /// <param name="friendlyName">Friendly Name for the Credential</param>
+        /// <param name="certificate">Certificate</param>
+        /// <param name="privateKey">Private Key</param>
+        /// <param name="sandbox">Flag denotes if it is Sandbox or not</param>
+        /// <param name="apiKey">API Key</param>
+        public virtual Credential ModifyCredential(string credentialSid,
+          string type, string friendlyName, string certificate,
+          string privateKey, string sandbox, string apiKey)
+        {
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/Credentials/{CredentialSid}";
+
+            request.AddUrlSegment("CredentialSid", credentialSid);
+
+            request.AddParameter("Type", type);
+            request.AddParameter("FriendlyName", friendlyName);
+            request.AddParameter("Attributes", attributes);
+
+            return Execute<Credential>(request);
+        }
+
+        /// <summary>
+        /// Deletes a Credential identified by Credential Sid.
+        /// </summary>
+        /// <param name="credentialSid">Credential Sid</param>
+        public virtual DeleteStatus DeleteCredential(string credentialSid)
+        {
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/Credentials/{CredentialSid}";
+            request.AddUrlSegment("CredentialSid", credentialSid);
+
+            var response = Execute(request);
+            return response.StatusCode == System.Net.HttpStatusCode.NoContent ?
+                                            DeleteStatus.Success :
+                                            DeleteStatus.Failed;
+        }
+    }
+}
