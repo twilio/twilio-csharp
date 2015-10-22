8bc07a4936761995f0bab5fc5f1ae7f9098a4302
Jingming Niu <niu@jingming.ca>
2015-10-22 15:01:46 -0700

    Add /Keys endpoints

---
+++ src/Twilio.Api.Tests.Integration/Model/KeyTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Twilio.Api.Tests.Integration.Model
+{
+    [TestFixture]
+    public class KeyTests
+    {
+        [Test]
+        public void testDeserializeResponse()
+        {
+            var doc = File.ReadAllText(Path.Combine("Resources", "key.json"));
+            var json = new JsonDeserializer();
+            json.DateFormat = "ddd, dd MMM yyyy HH:mm:ss '+0000'";
+            var output = json.Deserialize<Key>(new RestResponse { Content = doc });
+
+            Assert.NotNull(output);
+            Assert.NotNull(output.DateCreated);
+            Assert.AreEqual("Up4rxvMFIvbuV0elEqP9ZZDNRaujrklb", output.Secret);
+        }
+    }
+}
