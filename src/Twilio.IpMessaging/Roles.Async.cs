d8ee3815c18104771cfc08fe7ad258f8520a33c0
Justin Witz <jwitz@twilio.com>
2016-01-08 11:27:26 -0800

    Add creation and updating of roles

--- src/Twilio.IpMessaging/Roles.Async.cs
+++ src/Twilio.IpMessaging/Roles.Async.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using RestSharp;
 using RestSharp.Extensions;
 using RestSharp.Validation;
@@ -42,6 +43,59 @@ namespace Twilio.IpMessaging
 
             request.AddUrlSegment("ServiceSid", serviceSid);
             request.AddUrlSegment("RoleSid", roleSid);
+
+            ExecuteAsync<Role>(request, (response) => callback(response));
+        }
+
+        /// <summary>
+        /// Creates a Role
+        /// </summary>
+        /// <param name="serviceSid">Service Sid</param>
+        /// <param name="friendlyName">Friendly Name</param>
+        /// <param name="type">Type</param>
+        /// <param name="permissions">Permissions</param>
+        /// <returns>Role</returns>
+        public virtual void CreateRole(string serviceSid, string friendlyName, string type, List<string> permissions, Action<Role> callback)
+        {
+            Require.Argument("ServiceSid", serviceSid);
+            Require.Argument("FriendlyName", friendlyName);
+            Require.Argument("Type", type);
+
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/Services/{ServiceSid}/Roles";
+
+            request.AddUrlSegment("ServiceSid", serviceSid);
+            request.AddParameter("FriendlyName", friendlyName);
+            request.AddParameter("Type", type);
+            foreach (string permission in permissions) {
+                request.AddParameter("Permission", permission);
+            }
+
+            ExecuteAsync<Role>(request, (response) => callback(response));
+        }
+
+        /// <summary>
+        /// Updates a Role
+        /// </summary>
+        /// <param name="serviceSid">Service Sid</param>
+        /// <param name="roleSid">Role Sid</param>
+        /// <param name="friendlyName">Friendly Name</param>
+        /// <param name="permissions">Permissions</param>
+        /// <returns>Role</returns>
+        public virtual void UpdateRole(string serviceSid, string roleSid, string friendlyName, List<string> permissions, Action<Role> callback)
+        {
+            Require.Argument("ServiceSid", serviceSid);
+            Require.Argument("FriendlyName", friendlyName);
+
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/Services/{ServiceSid}/Roles/{RoleSid}";
+
+            request.AddUrlSegment("ServiceSid", serviceSid);
+            request.AddUrlSegment("RoleSid", roleSid);
+            request.AddParameter("FriendlyName", friendlyName);
+            foreach (string permission in permissions) {
+                request.AddParameter("Permission", permission);
+            }
 
             ExecuteAsync<Role>(request, (response) => callback(response));
         }
