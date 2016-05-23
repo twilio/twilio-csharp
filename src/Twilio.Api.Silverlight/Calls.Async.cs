5f30342031f46c5379d92a46061e3a47640d184a
Carlos Diaz-Padron <cdpadron@twilio.com>
2016-05-23 15:45:16 -0700

    Fix indentation

--- src/Twilio.Api.Silverlight/Calls.Async.cs
+++ src/Twilio.Api.Silverlight/Calls.Async.cs
@@ -167,15 +167,15 @@ namespace Twilio
 			if (options.Record) request.AddParameter("Record", "true");
 			if (options.SipAuthUsername.HasValue()) request.AddParameter("SipAuthUsername", options.SipAuthUsername);
 			if (options.SipAuthPassword.HasValue()) request.AddParameter("SipAuthPassword", options.SipAuthPassword);
-      if (options.RecordingStatusCallback.HasValue())
-      {
-          request.AddParameter("RecordingStatusCallback", options.RecordingStatusCallback);
-      }
-      if (options.RecordingStatusCallbackMethod.HasValue())
-      {
-        request.AddParameter("RecordingStatusCallbackMethod", options.RecordingStatusCallbackMethod); 
-      }
-      if (options.RecordingChannels.HasValue()) request.AddParameter("RecordingChannels", options.RecordingChannels);
+			if (options.RecordingStatusCallback.HasValue())
+			{
+			    request.AddParameter("RecordingStatusCallback", options.RecordingStatusCallback);
+			}
+			if (options.RecordingStatusCallbackMethod.HasValue())
+			{
+			  request.AddParameter("RecordingStatusCallbackMethod", options.RecordingStatusCallbackMethod); 
+			}
+			if (options.RecordingChannels.HasValue()) request.AddParameter("RecordingChannels", options.RecordingChannels);
 		}
 
 		/// <summary>
