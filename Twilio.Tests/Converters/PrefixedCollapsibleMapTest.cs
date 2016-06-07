2fdc75c047ad6219fa5a706ca062ee48ab50d479
svanweelden <svanweelden@twilio.com>
2016-06-07 12:22:24 -0700

    Adding C# addons logic

---
+++ Twilio.Tests/Converters/PrefixedCollapsibleMapTest.cs
@@ -0,0 +1,130 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using Twilio.Converters;
+using System.Collections.Generic;
+using ExtensionMethods;
+using System.Linq;
+
+namespace Twilio.Tests
+{
+	[TestFixture]
+	public class PrefixedCollapsibleMapTest : TwilioTest {
+
+		[Test]
+		public void TestEmptyMap() {
+
+			Dictionary<String, Object> inputDict = new Dictionary<String, Object>();
+			Dictionary<String, String> resultsDict = new Dictionary<String, String>();
+			List<String> prevList = new List<String>();
+
+			Dictionary<String, String> result = PrefixedCollapsibleMap.Flatten(
+				inputDict, 
+				resultsDict, 
+				prevList
+			);
+
+			Dictionary<String, String> expected = new Dictionary<String, String>();
+		
+			Assert.IsTrue(expected.SequenceEqual(result));
+		}
+
+		[Test]
+		public void TestBasicMap() {
+
+			Dictionary<String, Object> inputDict = new Dictionary<String, Object>();
+
+			inputDict.Add("foo", "bar");
+			inputDict.Add("cool", "beans"); 
+
+			Dictionary<String, String> resultsDict = new Dictionary<String, String>();
+			List<String> prevList = new List<String>();
+
+			Dictionary<String, String> result = PrefixedCollapsibleMap.Flatten(
+				inputDict, 
+				resultsDict, 
+				prevList
+			);
+
+			Dictionary<String, String> expected = new Dictionary<String, String>();
+			expected.Add("foo","bar");
+			expected.Add("cool","beans");
+
+			Assert.IsTrue(expected.SequenceEqual(result));
+		}
+
+		[Test]
+		public void TestComplexMap() {
+
+			Dictionary<String, Object> inputDict = new Dictionary<String, Object>();
+			Dictionary<String, Object> subDict = new Dictionary<String, Object>();
+
+			subDict.Add("cool", "people");
+			subDict.Add("fun", "times");
+
+			inputDict.Add("foo", "bar");
+			inputDict.Add("really", subDict); 
+
+			Dictionary<String, String> resultsDict = new Dictionary<String, String>();
+			List<String> prevList = new List<String>();
+
+			Dictionary<String, String> result = PrefixedCollapsibleMap.Flatten(
+				inputDict, 
+				resultsDict, 
+				prevList
+			);
+
+			Dictionary<String, String> expected = new Dictionary<String, String>();
+			expected.Add("foo","bar");
+			expected.Add("really.cool","people");
+			expected.Add("really.fun","times");
+
+			Assert.IsTrue(expected.SequenceEqual(result));
+		}
+
+		[Test]
+		public void TestEmptyNullSerialize() {
+
+			Dictionary<String, String> result = PrefixedCollapsibleMap.Serialize(
+				null, 
+				"really"
+			);
+
+			Dictionary<String, String> expected = new Dictionary<String, String>();
+
+			Assert.IsTrue(expected.SequenceEqual(result));
+
+			result = PrefixedCollapsibleMap.Serialize(
+				new Dictionary<String, Object>(), 
+				"really"
+			);
+
+			Assert.IsTrue(expected.SequenceEqual(result));
+		}
+
+		[Test]
+		public void TestNormalSerialize() {
+
+			Dictionary<String, Object> inputDict = new Dictionary<String, Object>();
+			Dictionary<String, Object> subDict = new Dictionary<String, Object>();
+
+			subDict.Add("cool", "people");
+			subDict.Add("fun", "times");
+
+			inputDict.Add("foo", "bar");
+			inputDict.Add("super", subDict); 
+
+			Dictionary<String, String> result = PrefixedCollapsibleMap.Serialize(
+				inputDict, 
+				"really"
+			);
+
+			Dictionary<String, String> expected = new Dictionary<String, String>();
+			expected.Add("really.foo","bar");
+			expected.Add("really.super.cool","people");
+			expected.Add("really.super.fun","times");
+
+			Assert.IsTrue(expected.SequenceEqual(result));
+		}
+	}
+}
---
+++ Twilio/Converters/PrefixedCollapsibleMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Twilio
+{
+	public class PrefixedCollapsibleMap
+	{
+		public static Dictionary<String, String> Flatten(
+			Dictionary<String, Object> dict, 
+			Dictionary<String, String> result,
+			List<String> previous
+		) {
+			foreach (KeyValuePair<String, Object> entry in dict) {
+				List<String> next = new List<String>(previous);
+				next.Add (entry.Key);
+
+				if (entry.Value.GetType() == typeof(Dictionary<String, Object>)) {
+					Flatten ((Dictionary<String, Object>)entry.Value, result, next);
+				} else {
+					result.Add(String.Join(".", next), entry.Value.ToString());
+				}
+			}
+
+			return result;
+		}
+
+		public static Dictionary<String, String> Serialize(
+			Dictionary<String, Object> inputDict,
+			String prefix
+		) {
+			if (inputDict == null || inputDict.Count() == 0) {
+				return new Dictionary<String, String>();
+			}
+				
+			Dictionary<String, String> flattened = Flatten(
+				inputDict, 
+				new Dictionary<String, String>(), 
+				new List<String>()
+			);
+
+			Dictionary<String, String> results = new Dictionary<String, String>();
+
+			foreach (KeyValuePair<String, String> entry in flattened) {
+				results.Add(prefix + "." + entry.Key, entry.Value);
+			}
+
+			return results;
+		}
+	}
+}
+
