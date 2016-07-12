1cde8043d135b4fc67c3b9d102b340ecb52beb23
Sergei Chertkov <schertkov@twilio.com>
2016-07-12 14:39:54 +0300

    Add Sync token grant

---
+++ Twilio/Auth/ConversationsGrant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Twilio.Auth
+{
+    public class ConversationsGrant : IGrant
+    {
+        public string ConfigurationProfileSid { get; set; }
+
+        public string GetGrantKey()
+        {
+            return "rtc";
+        }
+
+        public object GetPayload()
+        {
+            var payload = new Dictionary<string, string>();
+            if (this.ConfigurationProfileSid != null)
+            {
+                payload.Add("configuration_profile_sid", this.ConfigurationProfileSid);
+            }
+
+            return payload;
+        }
+    }
+}
---
+++ Twilio/Auth/IGrant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Twilio.Auth
+{
+    public interface IGrant
+    {
+        /// <summary>
+        /// Get the name of the grant.
+        /// </summary>
+        /// <returns>String - the name of the grant</returns>
+        string GetGrantKey();
+
+        /// <summary>
+        /// Get the data of the grant
+        /// </summary>
+        /// <returns>Object - the data of the grant</returns>
+        object GetPayload();
+    }
+}
