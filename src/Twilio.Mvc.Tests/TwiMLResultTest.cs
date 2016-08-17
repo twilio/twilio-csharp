864adcf4f7baaca0b45f4b0a489588a390b5249c
matt <matt@twilio.com>
2016-08-17 14:22:29 -0700

    Reconfigure TwiMLResult to prevent XXE
    TwiMLResult provides an additional (unused by the library)
    constructor that accepts a string value and will use
    `XDocument.Parse()` to turn it into an XML Document.
    
    Older versions of .Net allow DOCTYPE defined ENTITYs by default.
    Careless use of this class (passing in unsanitized user data) is
    vulnerable to an XXE Attack.
    
    This commit changes the default behavior to protect against this
    threat with associated positive and negative tests.
    
    For more information about this type of threat please see:
    https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Processing
    
    For .Net specific information and the reference solution this
    commit is based on please see:
    https://msdn.microsoft.com/en-us/magazine/ee335713.aspx

---
+++ src/Twilio.Mvc.Tests/TwiMLResultTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Twilio.TwiML.Mvc;
+using System.Xml;
+
+namespace Twilio.Mvc.Tests
+{
+    [TestFixture]
+    public class TwiMLResultTest : TestBase
+    {
+        [Test]
+        public void Can_Create_From_String()
+        {
+            const string payload = "<?xml version=\"1.0\"?>" +
+                                   "<Response><Say>test</Say></Response>";
+
+            var result = new TwiMLResult(payload);
+            var expected = XDocument.Parse(payload);
+
+            Assert.AreEqual(expected.ToString(), result.Data.ToString());
+        }
+
+        [Test]
+        public void Can_Reject_XXE_Payload()
+        {
+            const string xxePayload = "<?xml version=\"1.0\"?>\n" +
+                                      "<!DOCTYPE lolz [\n" +
+                                      "<!ENTITY lol \"lol\">\n" +
+                                      "<!ENTITY lol2 \"&lol;&lol;&lol;\">\n" +
+                                      "]>\n" +
+                                      "<lolz>&lol2;</lolz>";
+
+            Assert.Throws<XmlException>(delegate { new TwiMLResult(xxePayload); });
+        }
+    }
+}
