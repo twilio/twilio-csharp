00bd4f4ba230bd1c4a561e9a9c40e6665b0c49ad
Jingming Niu <niu@jingming.ca>
2016-11-04 11:09:20 -0700

    Fix exceptions

--- Twilio/Exceptions/RestException.cs
+++ Twilio/Exceptions/RestException.cs
@@ -1,29 +1,37 @@
-﻿using System;
+﻿using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 namespace Twilio.Exceptions
 {
-	public class RestException : TwilioException
+    [JsonObject(MemberSerialization.OptIn)]
+    public class RestException : TwilioException
 	{
 		[JsonProperty("code")]
-		public int Code { get; }
+		public int Code { get; private set; }
 
 	    [JsonProperty("status")]
-	    public int Status { get; }
+	    public int Status { get; private set; }
 
 	    [JsonProperty("message")]
-	    public override string Message { get; }
+	    public string Body { get; private set; }
 
-	    [JsonProperty("moreInfo")]
-		public string MoreInfo { get; }
+	    [JsonProperty("more_info")]
+		public string MoreInfo { get; private set; }
 
 	    public RestException() {}
-		public RestException(string message) : base(message) {}
-		public RestException(string message, Exception exception) : base(message, exception) {}
-		public RestException(int status, string message, int code, string moreInfo) : base(message) {
+	    private RestException(
+		    [JsonProperty("status")]
+		    int status,
+		    [JsonProperty("message")]
+		    string message,
+		    [JsonProperty("code")]
+		    int code,
+		    [JsonProperty("more_info")]
+		    string moreInfo
+		) {
 			Status = status;
-			Message = message;
 			Code = code;
+		    Body = message;
 			MoreInfo = moreInfo;
 		}
 
