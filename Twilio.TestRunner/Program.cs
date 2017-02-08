a9e3d902506e7d98f54a14a88c89bae8b5f8faa2
David Prothero <david@prothero.com>
2017-02-08 11:01:46 -0800

    Created .NET Core test runner

---
+++ Twilio.TestRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using NUnitLite;
+using Twilio.Tests;
+
+namespace Twilio.TestRunner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return new AutoRun(typeof(TwilioTest).GetTypeInfo().Assembly).Execute(args);
+        }
+    }
+}
