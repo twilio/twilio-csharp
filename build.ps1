5335814c3cf3d7c68518fb348f98e5be3d409765
David Prothero <david@prothero.com>
2017-02-13 16:15:24 -0800

    Added net451 target

--- build.ps1
+++ build.ps1
@@ -1,6 +1,8 @@
 msbuild /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
+# .NET Framework 3.5 Target & Tests
+
 msbuild /t:restore /p:TargetFramework=net35 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
@@ -9,6 +11,19 @@ if ($lastExitCode -ne 0) { exit $lastExitCode }
 
 .\test\Twilio.Test\bin\Release\net35\Twilio.Test.exe
 if ($lastExitCode -ne 0) { exit $lastExitCode }
+
+# .NET Framework 4.5.1 Target & Tests
+
+msbuild /t:restore /p:TargetFramework=net451 /verbosity:minimal
+if ($lastExitCode -ne 0) { exit $lastExitCode }
+
+msbuild /p:TargetFramework=net451 /p:Configuration=Release /verbosity:minimal
+if ($lastExitCode -ne 0) { exit $lastExitCode }
+
+.\test\Twilio.Test\bin\Release\net451\Twilio.Test.exe
+if ($lastExitCode -ne 0) { exit $lastExitCode }
+
+# .NET Standard 1.4 Target & Tests
 
 msbuild .\src\Twilio\Twilio.csproj /t:restore /p:TargetFramework=netstandard1.4 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
@@ -24,6 +39,8 @@ if ($lastExitCode -ne 0) { exit $lastExitCode }
 
 dotnet run --framework netcoreapp1.1 --project .\test\Twilio.Test\Twilio.Test.csproj
 if ($lastExitCode -ne 0) { exit $lastExitCode }
+
+# Create the NuGet Package
 
 msbuild .\src\Twilio\Twilio.csproj /t:pack /p:Configuration=Release /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
