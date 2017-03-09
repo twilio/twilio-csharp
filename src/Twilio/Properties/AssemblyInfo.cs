5ac1cb67ce4e465f55be21989c5cb8258c841236
Jingming Niu <niu@jingming.ca>
2017-03-09 12:27:03 -0800

    Bump to version 5.1.0

--- src/Twilio/Properties/AssemblyInfo.cs
+++ src/Twilio/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.CompilerServices;
 
 internal class AssemblyInfomation
 {
-    public const string AssemblyInformationalVersion = "5.0.2";
+    public const string AssemblyInformationalVersion = "5.1.0";
 }
