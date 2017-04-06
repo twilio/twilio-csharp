d39eedceb71e669ecb636f710809d6d983639adb
Evan Fossier <efossier@twilio.com>
2017-04-06 15:57:35 -0700

    Update video grant with room key

--- src/Twilio/JWT/AccessToken/VideoGrant.cs
+++ src/Twilio/JWT/AccessToken/VideoGrant.cs
@@ -10,7 +10,13 @@ namespace Twilio.Jwt.AccessToken
         /// <summary>
         /// Configuration profile SID
         /// </summary>
+        [System.Obsolete("ConfigurationProfileSid is deprecated, use Room instead")]
         public string ConfigurationProfileSid { get; set; }
+
+        /// <summary>
+        /// Roome SID or name
+        /// </summary>
+        public string Room { get; set; }
 
         /// <summary>
         /// Get the Video grant key
@@ -38,6 +44,10 @@ namespace Twilio.Jwt.AccessToken
                 if (ConfigurationProfileSid != null)
                 {
                     payload.Add("configuration_profile_sid", ConfigurationProfileSid);
+                }
+                if (Room != null)
+                {
+                    payload.Add("room", Room);
                 }
 
                 return payload;
