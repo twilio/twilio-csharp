39dfe7da5ae16e1539ccf97463582938b2c00d68
Evan Fossier <evan.fossier@gmail.com>
2017-04-13 12:59:27 -0700

    Release 5.2.2-alpha1

--- src/Twilio/Properties/AssemblyInfo.cs
+++ src/Twilio/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.CompilerServices;
 
 internal class AssemblyInfomation
 {
-    public const string AssemblyInformationalVersion = "5.2.1-alpha1";
+    public const string AssemblyInformationalVersion = "5.2.2-alpha1-alpha1";
 }
