7170eea4840b71ea73c5b3f9939f13402865197f
mplacona <marcos.placona@gmail.com>
2017-07-31 20:53:11 +0100

    Add missing Gather attribute. Fixes #379

--- test/Twilio.Test/TwiML/GatherTest.cs
+++ test/Twilio.Test/TwiML/GatherTest.cs
@@ -50,5 +50,145 @@ namespace Twilio.Tests.TwiML
               "</Response>"
           );
         }
+        
+        [Test]
+        public void TestPartialResultCallbackGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", partialResultCallback: "http://some_url_here");
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" partialResultCallback=\"http://some_url_here\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
+        
+        [Test]
+        public void TestPartialResultCallbackMethodGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", partialResultCallbackMethod: "POST");
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" partialResultCallbackMethod=\"POST\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
+        
+        [Test]
+        public void TestLanguageGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", language: "en-gb");
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" language=\"en-gb\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
+        
+        [Test]
+        public void TestHintsGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", hints: "words, phrases that have many words");
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" hints=\"words, phrases that have many words\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
+        
+        [Test]
+        public void TestProfanityFilterGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", profanityFilter: false);
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" profanityFilter=\"false\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
+        
+        [Test]
+        public void TestSpeechTimeoutGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", speechTimeout: 10);
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" speechTimeout=\"10\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
+        
+        [Test]
+        public void TestBargeInGather()
+        {
+            var g = new Gather(input:"speech", action:"/finalresult", bargeIn: false);
+            g.Say("Hello");
+            
+            var vr = new VoiceResponse();
+            vr.Gather(g);
+
+            Assert.AreEqual(
+                vr.ToString(),
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine +
+                "<Response>" + Environment.NewLine +
+                "  <Gather input=\"speech\" action=\"/finalresult\" bargeIn=\"false\">" + Environment.NewLine +
+                "    <Say>Hello</Say>" + Environment.NewLine +
+                "  </Gather>" + Environment.NewLine +
+                "</Response>"
+            );
+        }
     }
 }
