9c7f35aa3f4ad2e89b599e6a6171e1d860716f47
Jingming Niu <niu@jingming.ca>
2017-08-11 15:34:13 -0700

    Add ChatGrant, deprecate IpMessagingGrant (#383)

---
+++ src/Twilio/JWT/AccessToken/ChatGrant.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+
+namespace Twilio.Jwt.AccessToken
+{
+    /// <summary>
+    /// Grant to use for Twilio Chat
+    /// </summary>
+    public class ChatGrant : IGrant
+    {
+        /// <summary>
+        /// Service SID
+        /// </summary>
+        public string ServiceSid { get; set; }
+        
+        /// <summary>
+        /// Endpoint ID
+        /// </summary>
+        public string EndpointId { get; set; }
+        
+        /// <summary>
+        /// Deployment role SID
+        /// </summary>
+        public string DeploymentRoleSid { get; set; }
+        
+        /// <summary>
+        /// Push credential SID
+        /// </summary>
+        public string PushCredentialSid { get; set; }
+
+        /// <summary>
+        /// Get the grant name
+        /// </summary>
+        ///
+        /// <returns>name of the grant</returns>
+        public string Key
+        {
+            get
+            {
+                return "chat";
+            }
+        }
+
+        /// <summary>
+        /// Get the grant payload
+        /// </summary>
+        ///
+        /// <returns>payload of the grant</returns>
+        public object Payload
+        {
+            get
+            {
+                var payload = new Dictionary<string, string>();
+
+                if (ServiceSid != null)
+                {
+                    payload.Add("service_sid", ServiceSid);
+                }
+                if (EndpointId != null)
+                {
+                    payload.Add("endpoint_id", EndpointId);
+                }
+                if (DeploymentRoleSid != null)
+                {
+                    payload.Add("deployment_role_sid", DeploymentRoleSid);
+                }
+                if (PushCredentialSid != null)
+                {
+                    payload.Add("push_credential_sid", PushCredentialSid);
+                }
+
+                return payload;
+            }
+        }
+
+    }
+}
