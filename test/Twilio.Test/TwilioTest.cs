3508b90d538af57994210c21857c493cf117b666
Evan Fossier <evan.fossier@gmail.com>
2017-10-10 13:13:14 -0700

    Support serializing more types in tests

--- test/Twilio.Test/TwilioTest.cs
+++ test/Twilio.Test/TwilioTest.cs
@@ -6,6 +6,8 @@ namespace Twilio.Tests
     {
         public string Serialize(object obj) => obj.ToString();
         public string Serialize(DateTime date) => date.ToString("yyyy-MM-dd");
+        public string Serialize(bool boolean) => boolean.ToString().ToLower();
+        public string Serialize(Uri url) => url.AbsoluteUri.TrimEnd('/');
     }
 }
 
