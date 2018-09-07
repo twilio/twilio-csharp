13a22371f9b6f62a474a1f805f5f6a644ce2f850
Twilio <twilio-ci@twilio.com>
2018-09-07 14:44:33 -0700

    Release 5.16.4

--- src/Twilio/Properties/AssemblyInfo.cs
+++ src/Twilio/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.CompilerServices;
 
 internal class AssemblyInfomation
 {
-    public const string AssemblyInformationalVersion = "5.16.3";
+    public const string AssemblyInformationalVersion = "5.16.4";
 }
