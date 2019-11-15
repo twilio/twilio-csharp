2cfb43b90c207fb87d2cb07fd45070dbb3f5217c
Craig Dennis <cdennis@twilio.com>
2019-11-14 22:07:34 -0800

    Adds Twiml interface to assist with Type based overloading.

---
+++ src/Twilio/Types/ITwiml.cs
@@ -0,0 +1,15 @@
+namespace Twilio.Types
+{
+    /// <summary>
+    /// Twiml
+    /// </summary>
+    public interface ITwiml
+    {
+        /// <summary>
+        /// Generate string representation
+        /// </summary>
+        /// <returns>String representation</returns>
+        string ToString();
+    }
+}
+
