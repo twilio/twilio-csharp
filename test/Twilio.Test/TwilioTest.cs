fc21488af50f272752b44302f1f964e45a446fb0
Sam Harrison <sharrison@twilio.com>
2020-04-07 16:38:13 -0500

    update testing serializer

--- test/Twilio.Test/TwilioTest.cs
+++ test/Twilio.Test/TwilioTest.cs
@@ -1,13 +1,13 @@
 ﻿using System;
+using Twilio.Converters;
 
 namespace Twilio.Tests
 {
     public class TwilioTest
     {
-        public string Serialize(object obj) => obj.ToString();
+        public string Serialize(object obj) => Serializers.JsonObject(obj);
         public string Serialize(DateTime date) => date.ToString("yyyy-MM-dd");
         public string Serialize(bool boolean) => boolean.ToString().ToLower();
         public string Serialize(Uri url) => url.AbsoluteUri.TrimEnd('/');
     }
 }
-
