121250b92b561b3ccfa030c701aebcd9cea7a028
Twilio <twilio-ci@twilio.com>
2020-10-14 18:56:19 +0000

    Release 5.48.0

--- src/Twilio/Properties/AssemblyInfo.cs
+++ src/Twilio/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.CompilerServices;
 
 internal class AssemblyInfomation
 {
-    public const string AssemblyInformationalVersion = "5.47.2";
+    public const string AssemblyInformationalVersion = "5.48.0";
 }
