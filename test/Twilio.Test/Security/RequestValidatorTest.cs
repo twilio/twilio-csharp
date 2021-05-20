a9a0a37a05abac97bcf4e9c820c87f2abe49f183
Elmer Thomas <ethomas@twilio.com>
2021-05-20 11:45:56 -0700

    update tests

--- test/Twilio.Test/Security/RequestValidatorTest.cs
+++ test/Twilio.Test/Security/RequestValidatorTest.cs
@@ -35,14 +35,6 @@ namespace Twilio.Tests.Security
         public void TestValidateDictionaryMixedCase()
         {
             const string signature = "RSOYDt4T1cUTdK1PDd93/VVr8B8=";
-            const string url = "https://MyCompany.com:1234/MyApp.PHP?foo=1&bar=2";
-            Assert.IsFalse(_validator.Validate(url, _parameters, signature), "Request should have failed validation but didn't");
-        }
-
-        [Test]
-        public void TestValidateDictionaryMixedCaseSuccess()
-        {
-            const string signature = "RSOYDt4T1cUTdK1PDd93/VVr8B8=";
             const string url = "https://MyCompany.com:1234/myapp.php?foo=1&bar=2";
             Assert.IsTrue(_validator.Validate(url, _parameters, signature), "Request should have passed validation but didn't");
         }
