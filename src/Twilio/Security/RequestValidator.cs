80381f91694a146066732c61ec280b9b74dcce0e
Elmer Thomas <ethomas@twilio.com>
2021-05-24 14:56:52 -0700

    cleanup

--- src/Twilio/Security/RequestValidator.cs
+++ src/Twilio/Security/RequestValidator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Security.Cryptography;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Twilio.Security
 {
