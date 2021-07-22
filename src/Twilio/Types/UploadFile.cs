4017ae267ce6b9915ee1e162b79c4fcbaa78da34
Andrew Tsekhansky <atsekhansky@janison.com.au>
2021-07-23 09:13:34 +1200

    Add ability to upload supporting document content

---
+++ src/Twilio/Types/UploadFile.cs
@@ -0,0 +1,24 @@
+﻿#if !NET35
+using System.IO;
+
+namespace Twilio.Types
+{
+    public interface IUploadFile
+    {
+        string FileName { get; }
+        Stream Stream { get; }
+    }
+
+    public class UploadFile: IUploadFile
+    {
+        public UploadFile(string fileName, Stream stream)
+        {
+            FileName = fileName;
+            Stream = stream;
+        }
+
+        public string FileName { get; private set; }
+        public Stream Stream { get; private set; }
+    }
+}
+#endif
