42166fc46b44104c228b62d01f52b7eeb101aaa9
Shubham Tiwari <stiwari@twilio.com>
2023-09-18 11:12:56 +0530

    fix: added contentType and removed unused abstract function GetParam

---
+++ src/Twilio/Constant/EnumConstants.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+using Twilio.Converters;
+using Twilio.Types;
+
+namespace Twilio.Constant
+{
+    public class EnumConstants
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public sealed class ContentTypeEnum : StringEnum
+        {
+            private ContentTypeEnum(string value) : base(value) {}
+            public static readonly ContentTypeEnum JSON = new ContentTypeEnum("application/json");
+            public static readonly ContentTypeEnum FORM_URLENCODED = new ContentTypeEnum("application/x-www-form-urlencoded");
+        }
+    }
+}
