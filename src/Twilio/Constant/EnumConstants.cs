5f5bd13500e5528217a709f4f905c823f91896f9
Shubham <tiwarishubham635@gmail.com>
2023-09-18 16:41:40 +0530

    fix: removed unused constructor (#697)

--- src/Twilio/Constant/EnumConstants.cs
+++ src/Twilio/Constant/EnumConstants.cs
@@ -10,10 +10,6 @@ namespace Twilio.Constant
         public sealed class ContentTypeEnum : StringEnum
         {
             private ContentTypeEnum(string value) : base(value) {}
-            public static implicit operator ContentTypeEnum(string value)
-            {
-                return new ContentTypeEnum(value);
-            }
             public static readonly ContentTypeEnum JSON = new ContentTypeEnum("application/json");
             public static readonly ContentTypeEnum FORM_URLENCODED = new ContentTypeEnum("application/x-www-form-urlencoded");
         }
