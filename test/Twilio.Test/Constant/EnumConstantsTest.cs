e0cadb407b46a57ab8a3705ebd49001d0a4b7d39
Shubham <tiwarishubham635@gmail.com>
2023-09-18 12:52:54 +0530

    fix: added contentType and removed unused abstract function GetParam (#695)

--- test/Twilio.Test/Constant/EnumConstantsTest.cs
+++ test/Twilio.Test/Constant/EnumConstantsTest.cs
@@ -6,13 +6,12 @@ namespace Twilio.Tests.Constant
     [TestFixture]
     public class EnumConstantsTest
     {
-        
         [Test]
         public void EnumConstantComparison()
         {
             var jsonContentType = EnumConstants.ContentTypeEnum.JSON;
             var formUrlEncodedType = EnumConstants.ContentTypeEnum.FORM_URLENCODED;
-            
+          
             Assert.AreEqual("application/json", jsonContentType.ToString());
             Assert.AreEqual("application/x-www-form-urlencoded", formUrlEncodedType.ToString());
         }
