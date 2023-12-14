4847b688b1cdb17c0e377c897ddf5e34786407ef
Twilio <team_interfaces+github@twilio.com>
2023-12-14 10:40:11 +0000

    Release 6.16.0

--- src/Twilio/Properties/AssemblyInfo.cs
+++ src/Twilio/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.CompilerServices;
 
 internal class AssemblyInfomation
 {
-    public const string AssemblyInformationalVersion = "6.15.2";
+    public const string AssemblyInformationalVersion = "6.16.0";
 }
