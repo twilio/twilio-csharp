90de7b5ccc2272c580260cd2ca38c58ed43a050c
Shubham <tiwarishubham635@gmail.com>
2024-02-06 15:18:57 +0530

    Update SystemNetHttpClientTest.cs

--- test/Twilio.Test/Http/SystemNetHttpClientTest.cs
+++ test/Twilio.Test/Http/SystemNetHttpClientTest.cs
@@ -241,8 +241,7 @@ namespace Twilio.Tests.Http
             Assert.IsNotNull(internalRequest.Headers.UserAgent);
             Regex rgx = new Regex(@"^twilio-csharp/[0-9.]+(-rc\.[0-9]+)?\s\(\w+\s\w+\)\s[.\s\w]+/[^\s]+$");
             // debug statements
-            System.Console.WriteLine("Regex ------------------------------------------ " + rgx);
-            System.Console.WriteLine("internalRequest.Headers.UserAgent.ToString() ------------------------------------------ " + internalRequest.Headers.UserAgent.ToString());
+            Assert.AreEqual("abc", internalRequest.Headers.UserAgent.ToString());
             Assert.IsTrue(rgx.IsMatch(internalRequest.Headers.UserAgent.ToString()));
         }
 
