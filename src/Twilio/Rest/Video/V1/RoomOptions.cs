22c2c3a426539d614431040409eadf9853800646
Twilio <team_interfaces+github@twilio.com>
2024-10-17 14:05:50 +0000

    [Librarian] Regenerated @ d97b0740a0c7cf86beebd56c6973927b07719cde 1fc3bb6195ebcbd02451fb8fc9080c695b075d4f

--- src/Twilio/Rest/Video/V1/RoomOptions.cs
+++ src/Twilio/Rest/Video/V1/RoomOptions.cs
@@ -37,28 +37,28 @@ namespace Twilio.Rest.Video.V1
         ///<summary> An application-defined string that uniquely identifies the resource. It can be used as a `room_sid` in place of the resource's `sid` in the URL to address the resource, assuming it does not contain any [reserved characters](https://tools.ietf.org/html/rfc3986#section-2.2) that would need to be URL encoded. This value is unique for `in-progress` rooms. SDK clients can use this name to connect to the room. REST API clients can use this name in place of the Room SID to interact with the room as long as the room is `in-progress`. </summary> 
         public string UniqueName { get; set; }
 
-        ///<summary> The URL we should call using the `status_callback_method` to send status information to your application on every room event. See [Status Callbacks](https://www.twilio.com/docs/video/api/status-callbacks) for more info. </summary> 
+        ///<summary> The URL Twilio should call using the `status_callback_method` to send status information to your application on every room event. See [Status Callbacks](https://www.twilio.com/docs/video/api/status-callbacks) for more info. </summary> 
         public Uri StatusCallback { get; set; }
 
-        ///<summary> The HTTP method we should use to call `status_callback`. Can be `POST` or `GET`. </summary> 
+        ///<summary> The HTTP method Twilio should use to call `status_callback`. Can be `POST` or `GET`. </summary> 
         public Twilio.Http.HttpMethod StatusCallbackMethod { get; set; }
 
-        ///<summary> The maximum number of concurrent Participants allowed in the room. Peer-to-peer rooms can have up to 10 Participants. Small Group rooms can have up to 4 Participants. Group rooms can have up to 50 Participants. </summary> 
+        ///<summary> The maximum number of concurrent Participants allowed in the room. The maximum allowed value is 50. </summary> 
         public int? MaxParticipants { get; set; }
 
-        ///<summary> Whether to start recording when Participants connect. ***This feature is not available in `peer-to-peer` rooms.*** </summary> 
+        ///<summary> Whether to start recording when Participants connect. </summary> 
         public bool? RecordParticipantsOnConnect { get; set; }
 
-        ///<summary> An array of the video codecs that are supported when publishing a track in the room.  Can be: `VP8` and `H264`.  ***This feature is not available in `peer-to-peer` rooms*** </summary> 
+        ///<summary> An array of the video codecs that are supported when publishing a track in the room.  Can be: `VP8` and `H264`. </summary> 
         public List<RoomResource.VideoCodecEnum> VideoCodecs { get; set; }
 
-        ///<summary> The region for the media server in Group Rooms.  Can be: one of the [available Media Regions](https://www.twilio.com/docs/video/ip-addresses#group-rooms-media-servers). ***This feature is not available in `peer-to-peer` rooms.*** </summary> 
+        ///<summary> The region for the Room's media server.  Can be one of the [available Media Regions](https://www.twilio.com/docs/video/ip-addresses#group-rooms-media-servers). </summary> 
         public string MediaRegion { get; set; }
 
         ///<summary> A collection of Recording Rules that describe how to include or exclude matching tracks for recording </summary> 
         public object RecordingRules { get; set; }
 
-        ///<summary> When set to true, indicates that the participants in the room will only publish audio. No video tracks will be allowed. Group rooms only. </summary> 
+        ///<summary> When set to true, indicates that the participants in the room will only publish audio. No video tracks will be allowed. </summary> 
         public bool? AudioOnly { get; set; }
 
         ///<summary> The maximum number of seconds a Participant can be connected to the room. The maximum possible value is 86400 seconds (24 hours). The default is 14400 seconds (4 hours). </summary> 
