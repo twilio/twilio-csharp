6b275a4b619593dfc1a2d023d143878249c921aa
sbansla <104902068+sbansla@users.noreply.github.com>
2025-09-25 14:17:20 +0530

    chore: added App IEndpoint type (#794)
    * chore: added App IEndpoint type

---
+++ test/Twilio.Test/Types/AppTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Twilio.Exceptions;
+using Twilio.Types;
+
+namespace Twilio.Tests.Types
+{
+    [TestFixture]
+    public class AppTest
+    {
+        [Test]
+        public void TestToString()
+        {
+            Assert.AreEqual("app:me", new App("me").ToString());
+            Assert.AreEqual("app:YOU", new App("YOU").ToString());
+            Assert.AreEqual("APP:HIM", new App("APP:HIM").ToString());
+            Assert.AreEqual("aPp:her", new App("aPp:her").ToString());
+            Assert.Throws<ArgumentException>(() => new App("").ToString());
+            Assert.AreEqual("app:AP12345?mycustomparam1=foo&mycustomparam2=bar", new App("app:AP12345?mycustomparam1=foo&mycustomparam2=bar").ToString());
+            Assert.AreEqual("app:AP12345?mycustomparam1=foo&mycustomparam2=bar", new App("AP12345?mycustomparam1=foo&mycustomparam2=bar").ToString());
+        }
+    }
+}
