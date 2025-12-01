98640c890ba71c6afb65233e9556a29b00889d14
manisha1997 <singhmanisha.250025@gmail.com>
2025-12-01 16:33:13 +0530

    fix: Regional endpoint processing

--- src/Twilio/Clients/TwilioRestClient.cs
+++ src/Twilio/Clients/TwilioRestClient.cs
@@ -55,18 +55,6 @@ namespace Twilio.Clients
         private readonly string _password;
         private readonly AuthStrategy _authstrategy;
 
-                {
-                    { "au1", "sydney" },
-                    { "br1", "sao-paulo" },
-                    { "de1", "frankfurt" },
-                    { "ie1", "dublin" },
-                    { "jp1", "tokyo" },
-                    { "jp2", "osaka" },
-                    { "sg1", "singapore" },
-                    { "us1", "ashburn" },
-                    { "us2", "umatilla" }
-                };
-
         /// <summary>
         /// Constructor for a TwilioRestClient
         /// </summary>
